cfdf22957cbd5d11915ac1724ef75bb70eeb18ac
SuhagPersonal <Personal@Suhags-MacBook-Pro.local>
2020-05-12 18:38:01 +0530

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState("");
+  const [isLoading, toggleIsLoading] = useState(true);
+  const [isImage, toggleIsImage] = useState(true);
+
+  const getImage = async () => {
+    toggleIsLoading(true);
+    const response = await axios.get("https://random.dog/woof.json");
+    if(response.status === 200) {
+      setImageUrl(response.data.url);
+      if(response.data.url.includes(".mp4")) {
+        toggleIsImage(false);
+      } else {
+        toggleIsImage(true);
+      }
+      localStorage.setItem("imageUrl", response.data.url);
+      console.log(response.data.url);
+    } else {
+      alert("Error loading image. Please reload the page");
+    }
+  }
+
+  useEffect(() => {
+    if(!localStorage.getItem("imageUrl")) {
+      getImage();
+    } else {
+      let url = localStorage.getItem("imageUrl");
+      setImageUrl(url);
+      if(url.includes(".mp4")) {
+        toggleIsImage(false);
+      } else {
+        toggleIsImage(true);
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* { */}
+      <div style={{visibility:isLoading ? "visible" : "hidden"}}>Loading ...</div>
+      {
+        isImage ? 
+        <img className="image" style={{visibility:isLoading ? "hidden" : "visible"}} src={imageUrl} onLoad={() => {toggleIsLoading(false)}}/>
+        :
+        <video className="image" style={{visibility:isLoading ? "hidden" : "visible"}} src={imageUrl} onLoadStart={() => {toggleIsLoading(false)}} autoPlay/>
+      }
+      <br />
+      <button className="button" onClick = {getImage}>Load new image</button>
     </div>
   );
 }
